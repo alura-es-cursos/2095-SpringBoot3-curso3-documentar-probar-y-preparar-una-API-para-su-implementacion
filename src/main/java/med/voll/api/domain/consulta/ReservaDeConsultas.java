4fb06b0e1b53dfb5c906a58700676f60a37f6d66
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2024-06-08 19:24:57 -0300

    A1V3

---
+++ src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -0,0 +1,28 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservaDeConsultas {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public void reservar(DatosReservaConsulta datos){
+
+
+        var medico = medicoRepository.findById(datos.idMedico()).get();
+        var paciente = pacienteRepository.findById(datos.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, datos.fecha());
+        consultaRepository.save(consulta);
+    }
+}
