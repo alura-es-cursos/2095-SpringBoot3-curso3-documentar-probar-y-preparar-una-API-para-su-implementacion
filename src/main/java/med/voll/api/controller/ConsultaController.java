aa5cb5eeb38142230b2b8486705f485065adc15d
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2024-06-08 18:09:27 -0300

    A1V2

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,23 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import med.voll.api.domain.consulta.DatosReservaConsulta;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaController {
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos) {
+
+        return ResponseEntity.ok(new DatosDetalleConsulta(null, null, null, null));
+    }
+}
