8551a40e7dd745ced2f3cde1e50f773fd68af818
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2024-06-08 21:41:18 -0300

    A1V4

--- src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +21,21 @@ public class ReservaDeConsultas {
 
     public void reservar(DatosReservaConsulta datos){
 
+        if(!pacienteRepository.existsById(datos.idPaciente())){
+            throw new ValidacionException("No existe un paciente con el id informado");
+        }
 
-        var medico = medicoRepository.findById(datos.idMedico()).get();
+        if(datos.idMedico() != null && !medicoRepository.existsById(datos.idMedico())){
+            throw new ValidacionException("No existe un médico con el id informado");
+        }
+
+        var medico = elegirMedico(datos);
         var paciente = pacienteRepository.findById(datos.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, datos.fecha());
         consultaRepository.save(consulta);
+    }
+
+    private Medico elegirMedico(DatosReservaConsulta datos) {
+
     }
 }
